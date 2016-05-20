726136db8faa542cc73883e0acd699020720da9f
air <251949141@qq.com>
2016-05-20 22:02:31 +0800

    first commit

---
+++ lib/capistrano/scm/gulp.rb
@@ -0,0 +1,9 @@
+require "capistrano/scm/gulp/version"
+
+module Capistrano
+  module Scm
+    module Gulp
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/scm/gulp/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Scm
+    module Gulp
+      VERSION = "0.0.1"
+    end
+  end
+end
